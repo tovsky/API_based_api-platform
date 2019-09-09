9b00d7a3c95c67e5a124ecc4a4febeb1cb4b174e
tovsky <pavel.hr@mail.ru>
2019-09-09 19:15:43 +0300

    added short name and path

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -8,7 +8,10 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ApiResource(
  *     collectionOperations={"get", "post"},
- *     itemOperations={"get", "put"}
+ *     itemOperations={
+ *          "get"={"path"="/i_like_cheeses/{id}"},
+ *          "put"},
+ *     shortName="cheeses"
  * )
  * @ORM\Entity(repositoryClass="App\Repository\CheeseListingRepository")
  */
